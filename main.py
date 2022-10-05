46459d458734b7f89691a4f823da41460ea31e3f
Noel Brambila <noelbrambila@yahoo.com>
2022-10-04 17:51:16 -0700

    completed the ETL process

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import sqlite3
 from textwrap import indent
 from xml.etree.ElementTree import tostring
 # from flask import Flask
-# import sqlalchemy
+import sqlalchemy
 import pandas as pd 
-# from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker
 import requests
 import json
 from datetime import datetime
@@ -18,6 +18,8 @@ import datetime
 import base64
 
 IEX_API = "pk_913ba7d52f144907a92856b52ea0636e"
+DATABASE_LOCATION = "sqlite:///my_stock_quotes.sqlite"
+
 
 def getStockQuote(token, symbol):
     
@@ -32,18 +34,18 @@ def getStockQuote(token, symbol):
     return stock_quote
 
 def getStockQuoteData(stocks):
-    timestamps = datetime.datetime.now()
-    timestamps_y_m_d = timestamps.strftime("%Y-%m-%d")
+    time = datetime.datetime.now()
+    timestamp_y_m_d = time.strftime("%Y-%m-%d")
 
-    close = []  # iexClose
-    high = []   # week52High
-    low = []    # week52Low
-    open = []   # iexOpen
-    time = []   # latestTime
-    volume = [] # iexVolume
-    symbols = [] # symbol
-    
+    close = []      # iexClose
+    high = []       # week52High
+    low = []        # week52Low
+    open = []       # iexOpen
+    timestamp = []       # latestTime
+    volume = []     # iexVolume
+    symbols = []    # symbol
     quotes = {}
+
     for symbol in stocks:
         quotes[symbol] = getStockQuote(IEX_API, symbol)
         # Extracting only the relevant bits of data from the json object  
@@ -52,26 +54,75 @@ def getStockQuoteData(stocks):
             if key == "week52High": high.append(value)
             if key == "week52Low": low.append(value)
             if key == "iexOpen": open.append(value)
-            if key == "latestTime": time.append(timestamps_y_m_d)
+            if key == "latestTime": timestamp.append(timestamp_y_m_d)
             if key == "iexVolume": volume.append(value)
             if key == "symbol": symbols.append(value)
 
-    
-    
-    
     stock_quote_dict = {
         "close" : close,
         "high" : high,
         "low" : low,
         "open" : open,
-        "time" : time,
+        "timestamp" : timestamp,
         "volume" : volume,
+        "symbol" : symbols,
     }
 
     return stock_quote_dict
 
+def check_if_valid_data(df: pd.DataFrame) -> bool:
+    # Check if dataframe is empty
+    if df.empty:
+        print("No stock quotes downloaded. Finishing execution")
+        return False 
+
+    # Primary Key Check
+    if pd.Series(df['symbol']).is_unique:
+        pass
+    else:
+        raise Exception("Primary Key check is violated")
+
+    # Check for nulls
+    if df.isnull().values.any():
+        raise Exception("Null values found")
+
+    # Check that all timestamps are of yesterday's date
+    today = datetime.datetime.now()
+    today = today.strftime("%Y-%m-%d")
+
+
+    timestamps = df["timestamp"].tolist()
+    for timestamp in timestamps:
+        if datetime.datetime.strptime(timestamp, '%Y-%m-%d') == today:
+            raise Exception("At least one of the returned stock quotes does not have a yesterday's timestamp")
+
+    return True
+
+
 if __name__ == "__main__":
-    # print(getStockQuote(IEX_API, "aapl"))
     stocks = ['aapl', 'nke']
-    print(getStockQuoteData(stocks))
+    stock_quotes = getStockQuoteData(stocks)
+    stock_quotes_df = pd.DataFrame(stock_quotes, columns = ["symbol", "close", "high", "low", "open", "timestamp", "volume"])
+    
+    # Validate
+    if check_if_valid_data(stock_quotes_df):
+        print("Data valid, proceed to Load stage")
+
+    # Load
+    engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+    conn = sqlite3.connect('my_stock_quotes.sqlite')
+    cursor = conn.cursor()
+
+    print("Opened database successfully")
+
+    try:
+        stock_quotes_df.to_sql("my_stock_quotes", conn, index=False, if_exists='replace')
+    except:
+        print("Data already exists in the database")
+
+    df = pd.read_sql_query('SELECT * FROM my_stock_quotes', conn, parse_dates=["symbol"])
+
+    conn.close()
+    print("Close database successfully")
+    print(df.head)
 
