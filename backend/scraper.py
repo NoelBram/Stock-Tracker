fa985a25c82d94de79ec8baa62cde61ce373ffe4
Noel Brambila <noelbrambila@yahoo.com>
2022-12-17 00:19:02 -0800

    new stock api

--- backend/scraper.py
+++ backend/scraper.py
@@ -17,8 +17,13 @@ import streamlit as st
 import sqlite3
 from textwrap import indent
 from xml.etree.ElementTree import tostring
+from polygon import RESTClient
+from typing import cast
+from urllib3 import HTTPResponse
+from collections import ChainMap
 
-IEX_API = "pk_913ba7d52f144907a92856b52ea0636e"
+
+POLYGON_API_KEY = "3MF64zqXMDhh62DUOAC0Lduj7MpIXlmC"
 DATABASE_LOCATION = "sqlite:///db.sqlite"
 conn = sqlite3.connect('db.sqlite')
 
@@ -48,7 +53,7 @@ def getStockChartDatframe(stock, r):
     timestamp = []      # date
     volume = []         # volume
     
-    chart = getStockChart(IEX_API, stock, r)
+    chart = getStockChart(POLYGON_API, stock, r)
     # Extracting only the relevant bits of data from the json object  
     for date in chart:
         for key, value in date.items():    
@@ -116,19 +121,23 @@ def get_stock_chart_df(stock):
     print("Close database successfully")
     return df
 
-def getStockQuote(token, symbol):
-    endpoint = "https://cloud.iexapis.com/stable/stock/{symbol}/quote?token={token}".format(symbol = symbol, token = token)
-    headers = {
-        "Accept" : "application/json",
-        "Content-Type" : "application/json"}
+def getStockQuote(symbol, dateA, dataB):
+    client = RESTClient(POLYGON_API_KEY) # api_key is used
+    quotes = cast(
+        HTTPResponse,
+        client.get_aggs(
+            symbol,
+            1,
+            "day",
+            dateA,
+            dataB,
+            raw=True,
+        ),
+    ).data.decode('utf-8')   
+    return json.loads(quotes)
 
-    stock_quote_request = requests.get(endpoint, headers = headers)
-    stock_quote = stock_quote_request.json()
-    
-    return stock_quote
-
-# Clean the Data
-def getStockQuoteData(stocks):
+# Get many stocks and clean the data
+def getStockQuoteData(stocks, dateA, dataB):
     close = []      # iexClose
     high = []       # week52High
     low = []        # week52Low
@@ -139,23 +148,25 @@ def getStockQuoteData(stocks):
     quotes = {}
 
     for symbol in stocks:
-        quotes[symbol] = getStockQuote(IEX_API, symbol)
-        # Extracting only the relevant bits of data from the json object  
-        for key, value in quotes[symbol].items():    
-            if key == "iexClose": close.append(value)
-            if key == "week52High": high.append(value)
-            if key == "week52Low": low.append(value)
-            if key == "iexOpen": open.append(value)
-            if key == "latestTime": 
-                try: 
-                    timestamp.append(datetime.datetime.strptime(value, "%B %d, %Y").strftime("%Y-%m-%d"))
-                except ValueError:
-                    date = datetime.datetime.now()
-                    today = date.strftime("%Y-%m-%d")
-                    timestamp.append(today)
-            if key == "iexVolume": volume.append(value)
-            if key == "symbol": symbols.append(value)
-
+        quotes[symbol] = getStockQuote(symbol, dateA, dataB)
+        # Extracting only the relevant bits of data from the json object 
+    
+        symbols += [quotes[symbol].get('ticker')] * quotes[symbol].get('count')
+             
+        for quote in quotes[symbol].get('results')[:]:
+            close.append(quote["c"])
+            high.append(quote["h"])
+            low.append(quote["l"])
+            open.append(quote["o"])
+            open.append(quote["t"])
+                # try: 
+                #     timestamp.append(datetime.datetime.strptime(value, "%B %d, %Y").strftime("%Y-%m-%d"))
+                # except ValueError:
+                #     date = datetime.datetime.now()
+                #     today = date.strftime("%Y-%m-%d")
+                #     timestamp.append(today)
+            volume.append(quote["v"])
+                        
     stock_quote_dict = {
         "Close" : close,
         "High" : high,
@@ -211,13 +222,13 @@ def get_stock_df():
     return df
 
 if __name__ == '__main__':
-    # STOCK_NAME = 'aapl'
+    STOCK_NAME = 'AAPL'
+    STOCKS = ['AAPL', 'NKE']
+    # print(getStockQuote(STOCK_NAME, "2022-04-04", "2022-04-08"))
+    print(getStockQuoteData(STOCKS, "2022-04-04", "2022-04-08"))
     # print(get_stock_chart_df(STOCK_NAME))
-    # print(getStockChart(IEX_API, 'aapl', '10d'))
-    # print(getStockQuote(IEX_API, 'aapl'))
-    # stocks = ['aapl', 'nke']
-    # print(getStockQuoteData(stocks))
-    print(get_stock_df())
+    # print(getStockChart(RAPID_API, STOCK_NAME, '10d'))
+    # print(get_stock_df())
     
 
 
