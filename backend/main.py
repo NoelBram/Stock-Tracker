03a20de46b65cdf760c6243956e916d2d58e00c5
Noel Brambila <noelbrambila@yahoo.com>
2022-12-17 17:01:01 -0800

    Trasform and Load Processes Complete

--- backend/main.py
+++ backend/main.py
@@ -30,10 +30,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'pk_913ba7d52f144907a92856b52ea0636e'
 db = SQLAlchemy(app)
 
-stocks_df = get_stock_df()
-
-STOCK_NAME = 'aapl'
-df = get_stock_chart_df(STOCK_NAME)
+STOCK_NAME = 'AAPL'
+df = get_stock_df(STOCK_NAME, '2022-04-04', '2022-04-08')
 
 minmax = MinMaxScaler().fit(df.iloc[:, 4:5].astype('float32')) # Close index
 df_log = minmax.transform(df.iloc[:, 4:5].astype('float32')) # Close index
@@ -46,41 +44,41 @@ simulation_size = 10
 df_train = df_log.iloc[:-test_size]
 df_test = df_log.iloc[-test_size:]
 nfeatures = 7
-print("df.shape = {df}, df_train.shape = {df_train}, df_test.shape = {df_test}".format(df = df.shape, df_train = df_train.shape, df_test = df_test.shape))
+print('df.shape = {df}, df_train.shape = {df_train}, df_test.shape = {df_test}'.format(df = df.shape, df_train = df_train.shape, df_test = df_test.shape))
 
-class Model:
-    def __init__(
-        self,
-        learning_rate,
-        num_layers,
-        size,
-        size_layer,
-        output_size,
-        forget_bias = 0.1,
-    ):
-        tf.compat.v1.disable_eager_execution()
-        def lstm_cell(size_layer):
-            return layers.LSTMCell(size_layer)
+# class Model:
+#     def __init__(
+#         self,
+#         learning_rate,
+#         num_layers,
+#         size,
+#         size_layer,
+#         output_size,
+#         forget_bias = 0.1,
+#     ):
+#         tf.compat.v1.disable_eager_execution()
+#         def lstm_cell(size_layer):
+#             return layers.LSTMCell(size_layer)
 
-        rnn_cells = [lstm_cell(size_layer) for _ in range(num_layers)]
-        stacked_lstm = layers.StackedRNNCells(rnn_cells)
+#         rnn_cells = [lstm_cell(size_layer) for _ in range(num_layers)]
+#         stacked_lstm = layers.StackedRNNCells(rnn_cells)
 
-        self.X = tf.compat.v1.placeholder(tf.float32, (None, None, size))
-        self.Y = tf.compat.v1.placeholder(tf.float32, (None, output_size))
-        drop = tf.nn.RNNCellDropoutWrapper(
-            stacked_lstm, output_keep_prob = forget_bias
-        )
-        self.hidden_layer = tf.compat.v1.placeholder(
-            tf.float32, (None, num_layers * 2 * size_layer)
-        )
-        self.outputs, self.last_state = tf.nn.dynamic_rnn(
-            drop, self.X, initial_state = self.hidden_layer, dtype = tf.float32
-        )
-        self.logits = layers.Dense(self.outputs[-1], output_size)
-        self.cost = tf.compat.v1.reduce_mean(tf.square(self.Y - self.logits))
-        self.optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate).minimize(
-            self.cost
-        )
+#         self.X = tf.compat.v1.placeholder(tf.float32, (None, None, size))
+#         self.Y = tf.compat.v1.placeholder(tf.float32, (None, output_size))
+#         drop = tf.nn.RNNCellDropoutWrapper(
+#             stacked_lstm, output_keep_prob = forget_bias
+#         )
+#         self.hidden_layer = tf.compat.v1.placeholder(
+#             tf.float32, (None, num_layers * 2 * size_layer)
+#         )
+#         self.outputs, self.last_state = tf.nn.dynamic_rnn(
+#             drop, self.X, initial_state = self.hidden_layer, dtype = tf.float32
+#         )
+#         self.logits = layers.Dense(self.outputs[-1], output_size)
+#         self.cost = tf.compat.v1.reduce_mean(tf.square(self.Y - self.logits))
+#         self.optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate).minimize(
+#             self.cost
+#         )
         
 def calculate_accuracy(real, predict):
     real = np.array(real) + 1
@@ -220,12 +218,23 @@ def get_results():
     x_range_future = np.arange(len(results[0]))
     plt.xticks(x_range_future[::30], date_ori[::30])
     
-    url = "/backend/static/charts/{stock_name}.png".format(stock_name = STOCK_NAME)
+    url = '/static/charts/{stock_name}.png'.format(stock_name = STOCK_NAME)
 
-    plt.savefig(url, format="png")
+    plt.savefig(url, format='png')
     # full_filename = os.path.join(app.config['CHART_FOLDER'], url)
     return url
 
+def get_df_graph(df):
+    plt.figure(figsize = (15, 5))
+    df.plot(kind = 'scatter',
+        x = 'Date',
+        y = 'Close',
+        color = 'red')
+
+    url = 'backend/static/charts/{stock_name}.png'.format(stock_name = STOCK_NAME.lower())
+
+    plt.savefig(url, format='png')
+    return url[7:]
 # @app.route('/api/data')
 # def data():
 #     query = User.query
@@ -273,14 +282,15 @@ def get_results():
 
 @app.route('/')
 def test_flask():
-    stocks = stocks_df.to_numpy()
-    # img_src = get_results()
+    stocks = df.to_numpy()
+    # img_src = get_df_graph(df)
     img_src = None
-    return render_template('results.html', title='IEX Trading', stocks = stocks, img_src= img_src)
+    return render_template('results.html', title='Stock Price Forcasting App', stocks = stocks, img_src= img_src)
     # return render_template('results.html', title='IEX Trading')
 
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+    print(get_df_graph(df))
+    app.run(host='0.0.0.0', port=80)
     # image_url = get_results()
--- backend/scraper.py
+++ backend/scraper.py
@@ -23,92 +23,15 @@ from urllib3 import HTTPResponse
 from collections import ChainMap
 
 
-POLYGON_API_KEY = "3MF64zqXMDhh62DUOAC0Lduj7MpIXlmC"
-DATABASE_LOCATION = "sqlite:///db.sqlite"
+POLYGON_API_KEY = '3MF64zqXMDhh62DUOAC0Lduj7MpIXlmC'
+DATABASE_LOCATION = 'sqlite:///db.sqlite'
 conn = sqlite3.connect('db.sqlite')
 
 # Sidebar
 st.sidebar.subheader('Query parameters')
-start_date = st.sidebar.date_input("Start date", datetime.date(2019, 1, 1))
-end_date = st.sidebar.date_input("End date", datetime.date(2021, 1, 31))
+start_date = st.sidebar.date_input('Start date', datetime.date(2019, 1, 1))
+end_date = st.sidebar.date_input('End date', datetime.date(2021, 1, 31))
 
-
-def get_stock_chart_datframe(stock, r):
-    close = []          # close
-    adjClose = []    # fClose
-    high = []           # high
-    low = []            # lLow
-    open = []           # open
-    timestamp = []      # date
-    volume = []         # volume
-    
-    chart = get_stock_chart(POLYGON_API_KEY, stock, r)
-    # Extracting only the relevant bits of data from the json object  
-    for date in chart:
-        for key, value in date.items():    
-            if key == "close": close.append(value)
-            if key == "fClose": adjClose.append(value)
-            if key == "high": high.append(value)
-            if key == "low": low.append(value)
-            if key == "open": open.append(value)
-            if key == "date": timestamp.append(value)
-            if key == "volume": volume.append(value)
-
-    stock_chart_dict = {
-        "Close" : close,
-        "Adj Close" : adjClose,
-        "High" : high,
-        "Low" : low,
-        "Open" : open,
-        "Date" : timestamp,
-        "Volume" : volume,
-    }
-
-    stock_chart_df = pd.DataFrame(data=stock_chart_dict)
-
-    return stock_chart_df
-
-def check_if_valid_chart_data(df: pd.DataFrame) -> bool:
-    # Check if dataframe is empty
-    if df.empty:
-        print("No stock quotes downloaded. Finishing execution")
-        return False 
-
-    # Primary Key Check
-    if pd.Series(df['Date']).is_unique:
-        pass
-    else:
-        raise Exception("Primary Key check is violated: At least one of the returned stock quotes has the same date")
-
-    # Check for nulls
-    if df.isnull().values.any():
-        raise Exception("Null values found")
-
-    return True
-
-def get_stock_chart_df(stock):
-    # time = '10d'
-    time = '1y'
-    stock_chart_df = pd.DataFrame(get_stock_chart_datframe(stock, time), columns = ["Date", "Open", "High", "Low", "Close", "Adj Close", "Volume"])    
-    # Validate
-    if check_if_valid_chart_data(stock_chart_df):
-        print("Data valid, proceed to Load stage")
-    else: return pd.DataFrame()
-
-    # Load
-    print("Opened database successfully")
-
-    try:
-        stock_chart_df.to_sql("{stock}_chart".format(stock = stock), conn, index=False, if_exists='replace')
-    except:
-        print("Data already exists in the database")
-        return pd.DataFrame()
-    
-    df = pd.read_sql_query("SELECT * FROM {stock}_chart".format(stock = stock), conn, parse_dates=["date"])
-
-    conn.commit()
-    print("Close database successfully")
-    return df
 
 # Extract: get stock data
 def get_stock_quote(symbol, dateA, dataB):
@@ -118,7 +41,7 @@ def get_stock_quote(symbol, dateA, dataB):
         client.get_aggs(
             symbol,
             1,
-            "day",
+            'day',
             dateA,
             dataB,
             raw=True,
@@ -137,23 +60,23 @@ def get_stock_quote_data(symbol, dateA, dataB):
     # Extracting only the relevant bits of data from the json object 
     ticker = get_stock_quote(symbol, dateA, dataB)
     symbols = [ticker.get('ticker')] * ticker.get('count')
-    timestamp = pd.date_range(dateA, dataB).strftime("%Y-%m-%d").tolist()
+    timestamp = pd.date_range(dateA, dataB).strftime('%Y-%m-%d').tolist()
             
     for quote in ticker.get('results')[:]:
-        close.append(quote["c"])
-        high.append(quote["h"])
-        low.append(quote["l"])
-        open.append(quote["o"])
-        volume.append(quote["v"])
+        close.append(quote['c'])
+        high.append(quote['h'])
+        low.append(quote['l'])
+        open.append(quote['o'])
+        volume.append(quote['v'])
                         
     stock_quote_dict = {
-        "Close" : close,
-        "High" : high,
-        "Low" : low,
-        "Open" : open,
-        "Date" : timestamp,
-        "Volume" : volume,
-        "Symbol" : symbols,
+        'Close' : close,
+        'High' : high,
+        'Low' : low,
+        'Open' : open,
+        'Date' : timestamp,
+        'Volume' : volume,
+        'Symbol' : symbols,
     }
 
     return stock_quote_dict
@@ -161,52 +84,51 @@ def get_stock_quote_data(symbol, dateA, dataB):
 def check_if_valid_data(df: pd.DataFrame) -> bool:
     # Check if dataframe is empty
     if df.empty:
-        print("No stock quotes downloaded. Finishing execution")
+        print('No stock quotes downloaded. Finishing execution')
         return False 
 
     # Primary Key Check
-    if pd.Series(df['Symbol']).is_unique:
+    if pd.Series(df['Date']).is_unique:
         pass
     else:
-        raise Exception("Primary Key check is violated: At least one of the returned stock quotes has the same symblol")
+        raise Exception('Primary Key check is violated: At least one of the returned stock quotes has the same date.')
 
     # Check for nulls
     if df.isnull().values.any():
         print(df.head())
-        raise Exception("Null values found")
+        raise Exception('Null values found')
 
     return True
 
-def get_stock_df():
-    stocks = ['aapl', 'nke']
-    stock_quotes = get_stock_quote_data(stocks)
-    stock_quotes_df = pd.DataFrame(stock_quotes, columns = ["Symbol", "Date", "Open", "High", "Low", "Close", "Volume"])
+def get_stock_df(sybol, dateA, dataB):
+    stock_quotes = get_stock_quote_data(sybol, dateA, dataB)
+    stock_quotes_df = pd.DataFrame(stock_quotes, columns = ['Symbol', 'Date', 'Open', 'High', 'Low', 'Close', 'Volume'])
 
     # Validate
     if check_if_valid_data(stock_quotes_df):
-        print("Data valid, proceed to Load stage")
+        print('Data valid, proceed to Load stage')
 
     # Load
-    print("Opened database successfully")
+    print('Opened database successfully')
 
     try:
-        stock_quotes_df.to_sql("my_stock_quotes", conn, index=False, if_exists='replace')
+        stock_quotes_df.to_sql('my_stock_quotes', conn, index=False, if_exists='replace')
     except:
-        print("Data already exists in the database")
+        print('Data already exists in the database')
 
     df = pd.read_sql_query('SELECT * FROM my_stock_quotes', conn)
 
     conn.commit()
-    print("Close database successfully")
+    print('Close database successfully')
     return df
 
 if __name__ == '__main__':
     STOCK_NAME = 'AAPL'
-    STOCKS = ['AAPL', 'NKE']
-    # print(get_stock_quote(STOCK_NAME, "2022-04-04", "2022-04-08"))
-    print(get_stock_quote_data(STOCK_NAME, "2022-04-04", "2022-04-08"))
+#     STOCKS = ['AAPL', 'NKE']
+    # print(get_stock_quote(STOCK_NAME, '2022-04-04', '2022-04-08'))
+    # print(get_stock_quote_data(STOCK_NAME, '2022-04-04', '2022-04-08'))
 
-    # print(get_stock_df())
+    print(get_stock_df(STOCK_NAME, '2022-04-04', '2022-04-08'))
 
     # print(get_stock_chart_df(STOCK_NAME))
     # print(get_stock_chart(RAPID_API, STOCK_NAME, '10d'))
