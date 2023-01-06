399a63a6f182d51f7d234059f50489abd52372c9
Noel Brambila <noelbrambila@yahoo.com>
2023-01-05 22:39:34 -0800

    created model, need to reduce MSE

--- backend/main.py
+++ backend/main.py
@@ -30,14 +30,13 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
 from flask_sqlalchemy import SQLAlchemy
 from scraper import *
+from sklearn.model_selection import train_test_split
+
 
 CHARTS_FOLDER = os.path.join('static', 'charts')
 
 app = Flask(__name__)
-# app.config['CHART_FOLDER'] = CHARTS_FOLDER
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'pk_913ba7d52f144907a92856b52ea0636e'
-db = SQLAlchemy(app)
+app.config['CHART_FOLDER'] = CHARTS_FOLDER
 
 STOCK_NAME = 'AAPL'
 STOCKS = ['AAPL', 'NKE']
@@ -51,295 +50,111 @@ def prev_weekday(adate):
     return adate
 TODAY = prev_weekday(datetime.datetime.now())
 TODAY = TODAY.strftime("%Y-%m-%d")
-STOCK_LIST_DF = readSqliteTable('my_stock_list_quotes', None, None)
+print('\nHere is most recent weekday before today.: {day}\n'.format (day = TODAY))
 
 # STOCK_LIST_DF = pd.DataFrame()
 # for stock in STOCKS:
-#     print('Here is Today: \n')
-#     print(TODAY)
 #     STOCK_LIST_DF = get_stock_df('my_stock_list_quotes', stock, TODAY, TODAY)
 
-print('Here is a list of stock data of the most recent \'weekday\' before today.')
-print(STOCK_LIST_DF.to_numpy())
+STOCK_LIST_DF = readSqliteTable('my_stock_list_quotes', None, None)
 
-# Dataframe to graph out the forcast in dropdown.
+# print('Here is a list of stock data of the most recent \'weekday\' before today.')
+# print(STOCK_LIST_DF.to_numpy())
+
+# # Dataframe to graph out the forcast in dropdown.
+STOCK_DF = get_stock_df('my_stock_quotes', STOCK_NAME, '2014-12-23', TODAY)
 STOCK_DF = readSqliteTable('my_stock_quotes', None, None)
-# STOCK_DF = get_stock_df('my_stock_quotes', STOCK_NAME, '2021-12-23', TODAY)
 
-minmax = MinMaxScaler().fit(STOCK_DF.iloc[:, 4:5].astype('float32')) # Close index
-df_log = minmax.transform(STOCK_DF.iloc[:, 4:5].astype('float32')) # Close index
-df_log = pd.DataFrame(df_log)
-print(df_log.head())
+print('Here is stock data for {stock}.'.format(stock = STOCK_NAME))
+print(STOCK_DF.to_numpy())
+print(STOCK_DF.info())
 
-test_size = 30
-simulation_size = 10
+TEST_SIZE = 30
+RANDOM_STATE = 42
+OPTIMIZER = 'adam'
+LOSS = 'mean_squared_error'
+MODEL_TEST_SIZE = TEST_SIZE/258
+EPOCHS = 50
 
-df_train = df_log.iloc[:-test_size]
-df_test = df_log.iloc[-test_size:]
-nfeatures = 7
-print('STOCK_DF.shape = {df}, df_train.shape = {df_train}, df_test.shape = {df_test}'.format(df = STOCK_DF.shape, df_train = df_train.shape, df_test = df_test.shape))
+# @app.route('/results.png')
+# def get_results():
+if __name__ == '__main__':
+    # Extract the input features and target variable
+    input_values = STOCK_DF[['Open', 'High', 'Low', 'Close', 'Volume']]
+    target_value = STOCK_DF['Close']
 
-class Model:
-    def __init__(
-        self,
-        learning_rate,
-        num_layers,
-        size,
-        size_layer,
-        output_size,
-        forget_bias = 0.1,
-    ):
-        # tf.compat.v1.disable_eager_execution()
-        def lstm_cell(size_layer):
-            return layers.LSTMCell(size_layer)
+    # Calculate the variance of the target variable
+    variance = np.var(target_value)
+    target_value_summary = target_value.describe()
 
-        rnn_cells = [lstm_cell(size_layer) for _ in range(num_layers)]
-        stacked_lstm = layers.StackedRNNCells(rnn_cells)
+    X_train, X_test, y_train, y_test = train_test_split(input_values, target_value, test_size=MODEL_TEST_SIZE, random_state=RANDOM_STATE)
 
-        self.X = Input(shape=(size,))
-        self.Y = Dense(output_size, activation='softmax')(self.X)
-        # self.X = tf.compat.v1.placeholder(tf.float32, (None, None, size))
-        # self.Y = tf.compat.v1.placeholder(tf.float32, (None, output_size))
+    # Define the model
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Dense(units=64, activation='relu', input_shape=(X_train.shape[1],)))
+    model.add(tf.keras.layers.Dense(units=64, activation='relu'))
+    model.add(tf.keras.layers.Dense(units=64, activation='relu'))
+    model.add(tf.keras.layers.Dense(units=1))
 
-        drop = layers.RNN(stacked_lstm, return_sequences=True, return_state=True)
-        # drop = tf.nn.RNNCellDropoutWrapper(
-        #     stacked_lstm, output_keep_prob = forget_bias
-        # )
-        self.hidden_layer = Dense(num_layers * 2 * size_layer, activation='relu')(self.X)
-        # self.hidden_layer = tf.compat.v1.placeholder(
-        #     tf.float32, (None, num_layers * 2 * size_layer)
-        # )
+    # Compile the model
+    model.compile(optimizer=OPTIMIZER, loss=LOSS)
 
-        self.outputs, self.last_state = drop([self.X, self.hidden_layer])
-        # self.outputs, self.last_state = tf.nn.dynamic_rnn(
-        #     drop, self.X, initial_state = self.hidden_layer, dtype = tf.float32
-        # )
-        self.logits = layers.Dense(self.outputs, output_size)
-        self.cost = tf.compat.v1.reduce_mean(tf.square(self.Y - self.logits))
-        self.optimizer = keras.optimizers.Adam(learning_rate).minimize(
-            self.cost
-        )
-        
-def calculate_accuracy(real, predict):
-    real = np.array(real) + 1
-    predict = np.array(predict) + 1
-    percentage = 1 - np.sqrt(np.mean(np.square((real - predict) / real)))
-    return percentage * 100
+    # Train the model
+    history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=EPOCHS)
 
-def anchor(signal, weight):
-    buffer = []
-    last = signal[0]
-    for i in signal:
-        smoothed_val = last * weight + (1 - weight) * i
-        buffer.append(smoothed_val)
-        last = smoothed_val
-    return buffer
+    # Evaluate the model
+    mse = model.evaluate(X_test, y_test, verbose=0)
 
-def forecast():
-    num_layers = 1
-    size_layer = 128
-    timestamp = 5
-    epoch = 300
-    dropout_rate = 0.8
-    future_day = test_size
-    learning_rate = 0.01
-    
-    tf.compat.v1.reset_default_graph()
-    modelnn = Model(
-        learning_rate, num_layers, df_log.shape[1], size_layer, df_log.shape[1], dropout_rate
-    )
-    sess = tf.compat.v1.InteractiveSession()
-    sess.run(tf.compat.v1.global_variables_initializer())
-    date_ori = pd.to_datetime(STOCK_DF.iloc[:, 0]).tolist()
+    print('Here is the summary of the target varable. \n{sum}'.format(sum = target_value_summary))
+    print('mse      ', mse)
+    print('var      ', variance)
+    if mse < (variance*0.66):
+        print('Success, the MSE: {mse} is smaller than the Variance: {var}.'.format(mse= mse, var= variance))
+    else:
+        print('Fail, the MSE: {mse} is not much smaller than the Variance: {var}.'.format(mse= int(mse), var= int(variance)))
 
-    pbar = tqdm(range(epoch), desc = 'train loop')
-    for i in pbar:
-        init_value = np.zeros((1, num_layers * 2 * size_layer))
-        total_loss, total_acc = [], []
-        for k in range(0, df_train.shape[0] - 1, timestamp):
-            index = min(k + timestamp, df_train.shape[0] - 1)
-            batch_x = np.expand_dims(
-                df_train.iloc[k : index, :].values, axis = 0
-            )
-            batch_y = df_train.iloc[k + 1 : index + 1, :].values
-            logits, last_state, _, loss = sess.run(
-                [modelnn.logits, modelnn.last_state, modelnn.optimizer, modelnn.cost],
-                feed_dict = {
-                    modelnn.X: batch_x,
-                    modelnn.Y: batch_y,
-                    modelnn.hidden_layer: init_value,
-                },
-            )        
-            init_value = last_state
-            total_loss.append(loss)
-            total_acc.append(calculate_accuracy(batch_y[:, 0], logits[:, 0]))
-        pbar.set_postfix(cost = np.mean(total_loss), acc = np.mean(total_acc))
-    
-    future_day = test_size
+    #-----------------------------------------------------------
+    # Retrieve a list of list results on training and test data
+    # sets for each training epoch
+    #-----------------------------------------------------------
+    # loss = history.history['loss']
+    # val_loss = history.history['val_loss']
 
-    output_predict = np.zeros((df_train.shape[0] + future_day, df_train.shape[1]))
-    output_predict[0] = df_train.iloc[0]
-    upper_b = (df_train.shape[0] // timestamp) * timestamp
-    init_value = np.zeros((1, num_layers * 2 * size_layer))
-
-    for k in range(0, (df_train.shape[0] // timestamp) * timestamp, timestamp):
-        out_logits, last_state = sess.run(
-            [modelnn.logits, modelnn.last_state],
-            feed_dict = {
-                modelnn.X: np.expand_dims(
-                    df_train.iloc[k : k + timestamp], axis = 0
-                ),
-                modelnn.hidden_layer: init_value,
-            },
-        )
-        init_value = last_state
-        output_predict[k + 1 : k + timestamp + 1] = out_logits
-
-    if upper_b != df_train.shape[0]:
-        out_logits, last_state = sess.run(
-            [modelnn.logits, modelnn.last_state],
-            feed_dict = {
-                modelnn.X: np.expand_dims(df_train.iloc[upper_b:], axis = 0),
-                modelnn.hidden_layer: init_value,
-            },
-        )
-        output_predict[upper_b + 1 : df_train.shape[0] + 1] = out_logits
-        future_day -= 1
-        date_ori.append(date_ori[-1] + timedelta(days = 1))
-
-    init_value = last_state
-    
-    for i in range(future_day):
-        o = output_predict[-future_day - timestamp + i:-future_day + i]
-        out_logits, last_state = sess.run(
-            [modelnn.logits, modelnn.last_state],
-            feed_dict = {
-                modelnn.X: np.expand_dims(o, axis = 0),
-                modelnn.hidden_layer: init_value,
-            },
-        )
-        init_value = last_state
-        output_predict[-future_day + i] = out_logits[-1]
-        date_ori.append(date_ori[-1] + timedelta(days = 1))
-    
-    output_predict = minmax.inverse_transform(output_predict)
-    deep_future = anchor(output_predict[:, 0], 0.4)
-    
-    return deep_future
-
-@app.route('/results.png')
-def get_results():
-    results = []
-    for i in range(simulation_size):
-        print('simulation %d'%(i + 1))
-        results.append(forecast())
-    
-    date_ori = pd.to_datetime(df.iloc[:, 1]).tolist()
-    for i in range(test_size):
-        date_ori.append(date_ori[-1] + timedelta(days = 1))
-    date_ori = pd.Series(date_ori).dt.strftime(date_format = '%Y-%m-%d').tolist()
-    print(date_ori[-5:])
-
-    accepted_results = []
-    for r in results:
-        if (np.array(r[-test_size:]) < np.min(df['Close'])).sum() == 0 and \
-        (np.array(r[-test_size:]) > np.max(df['Close']) * 2).sum() == 0:
-            accepted_results.append(r)
-    # len(accepted_results)
-
-    accuracies = [calculate_accuracy(df['Close'].values, r[:-test_size]) for r in accepted_results]
-
-    fig, ax = plt.figure(figsize = (15, 5))
-    fig.patch.set_facecolor('#E8E5DA')
-    for no, r in enumerate(accepted_results):
-        plt.plot(r, label = 'forecast %d'%(no + 1))
-    plt.plot(df['Close'], label = 'true trend', c = 'black')
-    plt.legend()
-    plt.title('Average accuracy: %.4f'%(np.mean(accuracies)))
-
-    x_range_future = np.arange(len(results[0]))
-    plt.xticks(x_range_future[::30], date_ori[::30])
-
-    output = io.BytesIO()
-    FigureCanvas(fig).print_png(output)
-    
-    return Response(output.getvalue(), mimetype='image/png')
-
-sio = SocketIO(app)
-
-thread = None
-thread_lock = Lock()
-
-@app.route('/')
-@app.route('/results', methods=("POST", "GET"))
-def index():
-    global thread
-    return render_template('results.html', title='Stock Forcasting', stocks = STOCK_LIST_DF)
-
-# @app.route('/plot.png')
-# def plot_png():
 #     fig = create_figure(STOCK_DF)
+
 #     output = io.BytesIO()
 #     FigureCanvas(fig).print_png(output)
+    
 #     return Response(output.getvalue(), mimetype='image/png')
 
-# def create_figure(df):
+# def create_figure(df): 
 #     fig, ax = plt.subplots(figsize = (6,4))
 #     fig.patch.set_facecolor('#E8E5DA')
 
-#     x = df['Date']
-#     y = df['High']
+#     loss = df.history['loss']
+#     val_loss = df.history['val_loss']
 
-#     ax.bar(x, y, color = "#304C89")
+#     ax.bar(loss, val_loss, color = "#304C89")
 
 #     plt.ylabel('Stock Price (USD)', fontsize=8)
 #     plt.xlabel('Date (YYYY-MM-DD)', fontsize=8)
+#     plt.plot(EPOCHS, loss, 'r')
+#     plt.plot(EPOCHS, val_loss, 'b')
+#     plt.title('Training and validation accuracy')
+#     plt.xlabel("Epochs")
+#     plt.ylabel("Accuracy")
+#     plt.legend(["Accuracy", "Validation Accuracy"])
 
-#     return fig
-# @app.route('/api/data')
-# def data():
-#     query = User.query
+# sio = SocketIO(app)
 
-#     # search filter
-#     search = request.args.get('search[value]')
-#     if search:
-#         query = query.filter(db.or_(
-#             User.name.like(f'%{search}%'),
-#             User.email.like(f'%{search}%')
-#         ))
-#     total_filtered = query.count()
+# thread = None
+# thread_lock = Lock()
 
-#     # sorting
-#     order = []
-#     i = 0
-#     while True:
-#         col_index = request.args.get(f'order[{i}][column]')
-#         if col_index is None:
-#             break
-#         col_name = request.args.get(f'columns[{col_index}][data]')
-#         if col_name not in ['Symbol', 'Date', 'Date', 'High', 'Low', 'Close', 'Volume']:
-#             col_name = 'Symbol'
-#         descending = request.args.get(f'order[{i}][dir]') == 'desc'
-#         col = getattr(User, col_name)
-#         if descending:
-#             col = col.desc()
-#         order.append(col)
-#         i += 1
-#     if order:
-#         query = query.order_by(*order)
+# @app.route('/')
+# @app.route('/results', methods=("POST", "GET"))
+# def index():
+#     global thread
+#     return render_template('results.html', title='Stock Forcasting', stocks = STOCK_LIST_DF)
 
-#     # pagination
-#     start = request.args.get('start', type=int)
-#     length = request.args.get('length', type=int)
-#     query = query.offset(start).limit(length)
-
-#     # response
-#     return {
-#         'data': [user.to_dict() for user in query],
-#         'recordsFiltered': total_filtered,
-#         'recordsTotal': User.query.count(),
-#         'draw': request.args.get('draw', type=int),
-#     }
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug = True)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=80, debug = True)
