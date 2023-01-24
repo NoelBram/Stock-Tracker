984f095e1ff6536a7fd0fc1e42fa595ed79e6491
Noel Brambila <noelbrambila@yahoo.com>
2023-01-23 23:59:58 -0800

    adding window

--- backend/main.py
+++ backend/main.py
@@ -1,54 +1,45 @@
 #Flask imports
 from flask import Flask, render_template, send_file, make_response, url_for, Response
-import hyperopt
-from hyperopt import fmin, tpe, hp
+from flask_socketio import SocketIO
 
+# Data processing imports
 import sys
 import warnings
 if not sys.warnoptions:
     warnings.simplefilter('ignore')
-
-import os
-import io
 import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import statsmodels.api as sm
+from dateutil.rrule import rrule, DAILY, MO, TU, WE, TH, FR
+
+# Tensorflow imports
 import tensorflow as tf
 from tensorflow import keras
 from keras import backend, layers, metrics
 from keras.layers import Bidirectional, Dense, Dropout, Input, Bidirectional, RNN 
 from keras.losses import mean_squared_error
 
-import pandas as pd
+# Plotting imports
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
+# Other imports
 from datetime import date, datetime, timedelta
 from threading import Lock
-from flask import Flask, render_template
-from flask_socketio import SocketIO
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-
-from flask_sqlalchemy import SQLAlchemy
+import math
 from scraper import *
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-import statsmodels.api as sm
-from dateutil.rrule import rrule, DAILY, MO, TU, WE, TH, FR
-
-CHARTS_FOLDER = os.path.join('static', 'charts')
 
 app = Flask(__name__)
-app.config['CHART_FOLDER'] = CHARTS_FOLDER
-
 STOCK_NAME = 'AAPL'
 STOCKS = ['AAPL', 'NKE']
-YY = '2022'
-MM = '11'
-DD = '02'
+YY = '2021'
+MM = '01'
+DD = '20'
 
 IMAGE_URL = '/backend/static/assets/img/charts/{stock_name}.png'.format(stock_name = STOCK_NAME.lower())
 
@@ -60,7 +51,6 @@ def prev_weekday(adate):
     return adate
 TODAY = prev_weekday(datetime.datetime.now())
 TODAY = TODAY.strftime("%Y-%m-%d")
-print('\nHere is most recent weekday before today.: {day}\n'.format (day = TODAY))
 
 STOCK_LIST_DF = readSqliteTable('my_stock_list_quotes', None, None)
 if STOCK_LIST_DF is None:
@@ -77,34 +67,28 @@ if STOCK_DF is None:
     STOCK_DF = pd.DataFrame()
     STOCK_DF = get_stock_df('my_stock_quotes', STOCK_NAME, '{y}-{m}-{d}'.format(y = YY, m = MM, d = DD), TODAY)
 
-# Set the start date and end date
-start_date = datetime.datetime(int(YY), int(MM), int(DD))
-end_date = datetime.datetime(2023, 1, 6)
-
 # Pre-hyperparameters
 DAYS = 30
+start_date = datetime.datetime(int(YY), int(MM), int(DD))
+end_date = datetime.datetime.strptime(TODAY+' 00:00:00', "%Y-%m-%d %H:%M:%S")
 
 # Generate a range of weekdays (Monday through Friday)
 weekdays = list(rrule(DAILY, byweekday=(MO, TU, WE, TH, FR),dtstart=start_date, until=end_date))
 weekdays = [(datetime.datetime.strptime(weekday.strftime("%Y-%m-%d"), "%Y-%m-%d")).timestamp() for weekday in weekdays]
 future_weekdays = list(rrule(DAILY, byweekday=(MO, TU, WE, TH, FR), count=DAYS, dtstart=end_date))
 future_weekdays = [weekday.timestamp() for weekday in future_weekdays]
-
 # print('Last Week Days: ', weekdays[-10:])
 
-print('Here is stock data for {stock}.'.format(stock = STOCK_NAME))
-print(STOCK_DF.to_numpy())
-print(STOCK_DF.info())
-
 # Hyperparameters
-TEST_SIZE = 30
+TEST_SIZE = 60
 RANDOM_STATE = 42
 LOSS = 'mean_squared_error'
-MODEL_TEST_SIZE = TEST_SIZE/258
+MODEL_TEST_SIZE = 30/258
 EPOCHS = 50
 LEARNING_RATE = 0.000055
 REGL1 = 0.01
 REGL2 = 0.01
+WINDOW_SIZE = 30
 WEIGHT = 1.000001
 
 ARIMA_P = 2
@@ -146,7 +130,6 @@ class ARIMAModel:
         mse_test = ((y_pred_test - self.y_test) ** 2).mean()
 
         return mse_val, mse_test
-
 
 # ML Model using Liner Regresion
 class StockMLModel(tf.keras.Model):
@@ -218,19 +201,19 @@ def postprocess(forecasts):
 # if __name__ == '__main__':
 @app.route('/forecast.png')
 def get_forecast():
-    # Extract the input features and target variable
-    input_values = STOCK_DF[['Open', 'High', 'Low', 'Close', 'Volume', 'Date']]
-    target_value = STOCK_DF['Close']
+    # Extract the input features and target variable with GCD of rows to WINDOW_SIZE, hence '.iloc[5:]'.
+    input_values = STOCK_DF[['Open', 'High', 'Low', 'Close', 'Volume', 'Date']].iloc[5:]
+    target_value = STOCK_DF['Close'].iloc[5:]
 
     # Calculate the variance of the target variable
     variance = np.var(target_value)
-    target_value_summary = target_value.describe()
-    
+    print('The model input values for {stock} are as follows:\n{sum}\n'.format(stock = STOCK_NAME, sum = input_values.describe()))
+    print('The model target values for {stock} are as follows:\nvariance: {var}\n{sum}\n'.format(stock = STOCK_NAME, var = variance, sum = target_value.describe()))
+
     # Split the data into training, validation, and test sets
     X_train, X_test, y_train, y_test = train_test_split(input_values, target_value, test_size=0.1, random_state=RANDOM_STATE)
     X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.1, random_state=RANDOM_STATE)
     # Add more weight to the last input data
-    X_train.iloc[:, -1] = X_train.iloc[:, -1] * WEIGHT
 
     tf.random.set_seed(RANDOM_STATE)
     
@@ -240,15 +223,12 @@ def get_forecast():
     # Initialize the machine learning model, then compile 
     stock_model = StockMLModelWithTraining(X_train, y_train, X_val, y_val, X_test, y_test)
     stock_model.compile_and_fit(optimizer, LOSS, EPOCHS)
-
     ml_model = stock_model.model
     ml_forecasting = ml_model.predict(X_val)
-
     # print('The ML Model Forecast:\n', ml_forecasting)
 
     # Instantiate the model with the training, validation, and test sets
     arima_model = ARIMAModel(y_train, y_val, y_test)
-
     arima_model.fit(ARIMA_P, ARIMA_D, ARIMA_Q)
     arima__forecasting, conf_int = arima_model.forecast(days=DAYS)
     # print("The ARIMA Model Forecast:\n", arima__forecasting)
@@ -262,7 +242,7 @@ def get_forecast():
     else:
         result += 'Fail, the MSE: {mse} is not much smaller than the Variance: {var}.'.format(mse= int(mse), var= int(variance))
 
-        # Fit the model with p, d, and q values
+    # Fit the model with p, d, and q values
     arima_model.fit(ARIMA_P, ARIMA_D, ARIMA_Q)
     mse_val, mse_test = arima_model.evaluate()
 
@@ -270,18 +250,17 @@ def get_forecast():
     fig, ax = plt.subplots(figsize = (25,8))
     cd = target_value[-20:]
     ax.plot(weekdays[-len(cd):], cd, label='Current Data', color='black')
-    plt.plot(weekdays[-len(cd):], y_test[-20:], label='Y Test Vales', color='green')
+    # ax.plot(weekdays[-len(cd):], y_val[-20:], label='Y VAL Vales', color='green')
 
-    ax.plot([], [], label='ML Model: Mean Squared Error:  %.4f'%mse, alpha=0)
-    ax.plot(future_weekdays, ml_forecasting[:DAYS], label='Next Month\'s Forecast', color='blue')
-    ax.plot([], [], label='', alpha=0)
-
-    ax.plot([], [], label='ARIMA Model: Mean Squared Error on test set is {t} validation set is {v}'.format(t = int(mse_test), v= int(mse_val)), alpha=0)
-    # plt.fill_between(conf_int.index, conf_int.iloc[:,0], conf_int.iloc[:,1], color='pink')
-    plt.plot(future_weekdays, arima__forecasting, label='forecast')
-    ax.plot([], [], label='', alpha=0)
-
+    ax.plot(future_weekdays, ml_forecasting[:DAYS], label='ML Forecast', color='blue')
     ax.plot([], [], label=result, alpha=0)
+    ax.plot([], [], label='', alpha=0)
+    # plt.fill_between(conf_int.index, conf_int.iloc[:,0], conf_int.iloc[:,1], color='pink')
+
+    ax.plot(future_weekdays, arima__forecasting, label='AMIRA Forecast')
+    # ax.plot([], [], label='', alpha=0)
+    ax.plot([], [], label='ARIMA Model: MSE with the test set is {t} and with the validation set is {v}'.format(t = int(mse_test), v= int(mse_val)), alpha=0)
+
    
     ax.set_ylabel('Stock Price (USD)')
     ax.set_xlabel('Date (YYYY-MM-DD)')
@@ -295,72 +274,6 @@ def get_forecast():
     
     # Send the plot back to the client
     return send_file('temp.png', mimetype='image/png')
-
-    # Evaluate the model
-    # mse = ml_model.evaluate(X_test, y_test, verbose=0)
-
-    # print('Here is the summary of the target varable. \n{sum}'.format(sum = target_value_summary))
-    # print('mse      ', mse)
-    # print('var      ', variance)
-
-# if __name__ == '__main__':
-#     # Define the search space for the hyperparameters
-#     space = hp.choice('model_type', [
-#         {
-#             'type': 'linear',
-#             'regularization': hp.lognormal('regularization', 0, 1)
-#         },
-#         {
-#             'type': 'random_forest',
-#             'max_depth': hp.quniform('max_depth', 2, 10, 1),
-#             'min_samples_split': hp.quniform('min_samples_split', 2, 10, 1)
-#         }
-#     ])
-
-#     # Define the objective function to be minimized
-#     def objective(params):
-#         model = create_model(params)  # Function to create the model with the specified hyperparameters
-#         model.fit(X_train, y_train)
-#         y_pred = model.predict(X_val)
-#         return mean_squared_error(y_val, y_pred)
-
-#     # Run the optimization
-#     best = fmin(objective, space, algo=tpe.suggest, max_evals=100)
-
-#     # Use the optimal hyperparameters to create the final model
-#     optimal_model = create_model(best)
-#     optimal_model.fit(X_train, y_train)
-    #-----------------------------------------------------------
-    # Retrieve a list of list results on training and test data
-    # sets for each training epoch
-    #-----------------------------------------------------------
-    # loss = history.history['loss']
-    # val_loss = history.history['val_loss']
-
-#     fig = create_figure(STOCK_DF)
-
-#     output = io.BytesIO()
-#     FigureCanvas(fig).print_png(output)
-    
-#     return Response(output.getvalue(), mimetype='image/png')
-
-# def create_figure(df): 
-#     fig, ax = plt.subplots(figsize = (6,4))
-#     fig.patch.set_facecolor('#E8E5DA')
-
-#     loss = df.history['loss']
-#     val_loss = df.history['val_loss']
-
-#     ax.bar(loss, val_loss, color = "#304C89")
-
-#     plt.ylabel('Stock Price (USD)', fontsize=8)
-#     plt.xlabel('Date (YYYY-MM-DD)', fontsize=8)
-#     plt.plot(EPOCHS, loss, 'r')
-#     plt.plot(EPOCHS, val_loss, 'b')
-#     plt.title('Training and validation accuracy')
-#     plt.xlabel("Epochs")
-#     plt.ylabel("Accuracy")
-#     plt.legend(["Accuracy", "Validation Accuracy"])
 
 sio = SocketIO(app)
 
